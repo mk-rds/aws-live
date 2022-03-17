81ad2f0e0da4fcf92e6a32d51040b153e2c70607
mk-rds <71838012+mk-rds@users.noreply.github.com>
2022-03-17 14:40:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cvodm9afoekj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "tanmingkit-employee"
+customregion = "us-east-1"
